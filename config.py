9b2933328c54d16d5b8962eef4bb7a7fabaeaf0c
Cristobal <ortega.cortega@gmail.com>
2021-08-16 12:52:47 +0200

    adding last part

--- config.py
+++ config.py
@@ -2,14 +2,14 @@
 sql_extensions=['sql']
 
 # Path to review
-sql_path=r'Z:\Analysis\sql'
+sql_path=r'Z:\work\Analysis\temp\sql1'
 
 # JSON (data model) to create
 # You can use datetime to string formats between brackets to print the now() time in the name
 # f.e.: [%Y%m/%d/_%H%M%S]
-json_sql_model=r'Z:\Datamodel_tests\[%Y%m%d]_datamodelklarna.json'
+json_sql_model=r'Z:\work\Analysis\temp\sql1\[%Y%m%d]_datamodelklarna.json'
 
 
 # To avoid reinterpreting the name of the file, this needs to be fixed. No datetime format allowed
-json_sql_model_fixed=r'Z:\Datamodel_tests\20210322_datamodelklarna.json'
-plant_uml=r'Z:\Datamodel_tests\[%Y%m%d]_datamodelklarna.plantuml'
+json_sql_model_fixed=r'Z:\work\Analysis\temp\sql1\20210406_datamodelklarna.json'
+plant_uml=r'Z:\work\Analysis\temp\sql1\[%Y%m%d]_datamodelklarna.plantuml'
